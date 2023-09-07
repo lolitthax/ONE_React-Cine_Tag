248548d6f00409c7c6f2e6e84996dd540734f2f4
Aline Damião <aline.isilvadamiao@gmail.com>
2023-09-07 12:12:47 -0300

    criando cards e listas dinâmicas

--- cinetag/src/pages/Inicio/index.js
+++ cinetag/src/pages/Inicio/index.js
@@ -1,8 +1,10 @@
 import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
+import Card from "components/Card";
 import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
-
+import videos from 'json/db.json';
+import styles from './Inicio.module.css';
 function Inicio() {
     return (
         <>
@@ -12,6 +14,11 @@ function Inicio() {
             <Titulo>
                 <h1> Um lugar para guardar seus vídeos e filmes!</h1>
             </Titulo>
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id} />
+                })}
+            </section>
             <Rodape />
         </>
     )
